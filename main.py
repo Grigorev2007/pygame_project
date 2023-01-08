07f54dc932f43573a70fd688344b0971feff2eb9
Grigorev2007 <118687627+Grigorev2007@users.noreply.github.com>
2023-01-08 11:19:28 +0300

    Update main.py
    убрал некоторые коммы

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import pygame
 
-FPS = 50  # количество кадров в секунду
+FPS = 50
 SIZE = WIDTH, HEIGHT = 650, 700
 
 
 class Board:
-    # создание поля
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.board = [
             [0] * width for _ in range(height)
         ]
-        # значения по умолчанию
         self.left = 10
         self.top = 10
         self.cell_size = 30
 
-    # настройка внешнего вида
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
