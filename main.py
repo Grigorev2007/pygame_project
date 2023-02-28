c0db76d4145c09d97b7719cd6623b3a3b8a6c267
Grigorev2007 <118687627+Grigorev2007@users.noreply.github.com>
2023-02-28 21:51:13 +0300

    обнова
    полностью рабочая игра (+привидения, жизни, очки)

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import os
 import sys
 import pygame
+import math
+import time
 
 pygame.init()
 FPS = 20  # количество кадров в секунду
 SIZE = WIDTH, HEIGHT = 650, 700
 all_sprites = pygame.sprite.Group()
-x_pac = 1
-y_pac = 1
+x_cell = 1
+y_cell = 1
+x_pac = 30
+y_pac = 30
+x_ghost = 0
+y_ghost = 0
+counter = 0
+checker = 1
+lifes = 1
+time_start = 1000000000
+level = [["#", "#", "#", "#", "#", "#", "#", "#", "#", "*", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+         ["#", "-", "*", "*", "*", "*", "#", "#", "#", "*", "#", "#", "*", "*", "*", "#", "*", "*", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "#", "#", "#", "*", "#", "#", "*", "#", "*", "*", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "*", "*", "*", "*", "*", "*", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "#", "#", "#", "*", "#", "#", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "*", "*", "*", "*", "#", "#", "#", "*", "*", "*", "*", "*", "*", "*", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "#", "#", "#", "#", "#", "#", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "*", "*", "*", "#", "#", "#", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "*", "*", "*", "*", "#", "#", "*", "#", "#", "#", "*", "*", "*", "*", "*", "*", "*", "#"],
+         ["#", "#", "#", "*", "#", "#", "#", "*", "*", "#", "#", "#", "#", "#", "#", "#", "*", "#", "#", "#"],
+         ["#", "*", "*", "*", "*", "*", "#", "*", "#", "#", "#", "#", "*", "*", "*", "*", "*", "*", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "#", "*", "#", "#", "#", "#", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "*", "*", "#", "#", "#", "#", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "#", "*", "#", "#", "#", "#", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "*", "*", "*", "*", "#", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "#", "#", "#", "*", "#", "#", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "*", "*", "*", "*", "*", "*", "*", "#", "#", "#", "*", "#", "*", "#"],
+         ["#", "*", "#", "#", "#", "*", "#", "#", "#", "*", "#", "#", "*", "#", "*", "*", "*", "#", "*", "#"],
+         ["#", "*", "*", "*", "*", "*", "#", "#", "#", "*", "#", "#", "*", "*", "*", "#", "*", "*", "*", "#"],
+         ["#", "#", "#", "#", "#", "#", "#", "#", "#", "*", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]]
 
 
 def load_level(filename):
@@ -28,6 +58,46 @@ def load_image(name, colorkey=None):
         sys.exit()
     image = pygame.image.load(fullname)
     return image
+
+
+class Ghost(pygame.sprite.Sprite):
+    def __init__(self, x, y, color, image):
+        super().__init__()
+        self.image = image
+        self.image = pygame.transform.scale(self.image, (25, 25))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.direction = "right"
+        self.speed = 5
+
+    def upd(self, level, screen):
+        global x_pac, y_pac, lifes, time_start
+        if self.direction == "right":
+            if level[self.rect.x // 30][(self.rect.y - 80) // 30] != "#":
+                self.rect.x += self.speed
+            else:
+                self.direction = "left"
+        else:
+            if level[(self.rect.x - 31) // 30][(self.rect.y - 80) // 30] != "#":
+                self.rect.x -= self.speed
+            else:
+                self.direction = "right"
+        if self.rect.x - 49 <= x_pac <= self.rect.x - 11 and self.rect.y - 109 <= y_pac <= self.rect.y - 51 \
+                and lifes == 0:
+            if time.time() - time_start >= 3:
+                pygame.font.init()
+                my_font = pygame.font.SysFont(None, 60)
+                screen.fill("black")
+                text_surface = my_font.render('Жизни кончились :(!', False, (0, 0, 205))
+                screen.blit(text_surface, (140, 300))
+                pygame.display.update()
+                time.sleep(3)
+                sys.exit()
+        else:
+            if self.rect.x - 49 <= x_pac <= self.rect.x - 11 and self.rect.y - 109 <= y_pac <= self.rect.y - 51:
+                lifes -= 1
+                time_start = time.time()
 
 
 class Board:
@@ -64,7 +134,7 @@ class Board:
                     (x, y, self.cell_size, self.cell_size),
                     width=1
                 )
-                if self.level[a][b] != "*":
+                if self.level[a][b] == "#":
                     pygame.draw.rect(
                         screen,
                         (25, 25, 112),
@@ -78,66 +148,122 @@ class Board:
                         (x, y, self.cell_size, self.cell_size),
                         width=1
                     )
-                    pygame.draw.circle(
-                        screen,
-                        "yellow",
-                        (x + self.cell_size // 2, y + self.cell_size // 2),
-                        3)
+                    if self.level[a][b] == "*":
+                        pygame.draw.circle(
+                            screen,
+                            "yellow",
+                            (x + self.cell_size // 2, y + self.cell_size // 2),
+                            3)
                 b += 1
 
 
 class Pacman(pygame.sprite.Sprite):
-    image = load_image("pacman.png")
+    pacman_images = [pygame.transform.scale(load_image("pacman01.png"), (25, 25)),
+                     pygame.transform.scale(load_image("pacman02.png"), (25, 25)),
+                     pygame.transform.scale(load_image("pacman03.png"), (25, 25)),
+                     pygame.transform.scale(load_image("pacman04.png"), (25, 25))]
+
     def __init__(self, level, *group):
         super().__init__(*group)
-        self.image = Pacman.image
+        self.image = Pacman.pacman_images[0]
         self.level = level
         self.image = pygame.transform.scale(self.image, (22, 22))
         self.rect = self.image.get_rect()
         self.rect.center = (70, 120)
 
-    def update(self, *args):
-        global x_pac, y_pac
+    def update(self, screen, *args):
+        global counter, checker, x_cell, y_cell, x_pac, y_pac
+        counter += 1
+        if counter == 4:
+            counter = 0
+        self.image = Pacman.pacman_images[counter]
         if args:
             if args[0] == "up":
-                y_pac -= 1
-                if self.level[x_pac][y_pac] != "#":
-                    self.rect = self.rect.move(0, -30)
+                self.image = pygame.transform.rotate(self.image, 90)
+                y_pac -= 10
+                y_cell = math.floor(y_pac / 30)
+                if x_pac % 30 == 0 and y_pac % 30 == 0:
+                    if self.level[x_pac // 30][y_pac // 30] != "-":
+                        self.level[x_pac // 30][y_pac // 30] = "-"
+                        checker += 1
+                if x_pac % 30 == 0:
+                    x_cell = x_pac // 30
+                    if self.level[x_cell][y_cell] != "#":
+                        self.rect = self.rect.move(0, -10)
+                    else:
+                        y_pac += 10
                 else:
-                    y_pac += 1
+                    y_pac += 10
 
             if args[0] == "down":
-                y_pac += 1
-                if self.level[x_pac][y_pac] != "#":
-                    self.rect = self.rect.move(0, 30)
+                self.image = pygame.transform.rotate(self.image, 270)
+                y_pac += 10
+                y_cell = math.ceil(y_pac / 30)
+                if x_pac % 30 == 0 and y_pac % 30 == 0:
+                    if self.level[x_pac // 30][y_pac // 30] != "-":
+                        self.level[x_pac // 30][y_pac // 30] = "-"
+                        checker += 1
+                if x_pac % 30 == 0:
+                    x_cell = x_pac // 30
+                    if self.level[x_cell][y_cell] != "#":
+                        self.rect = self.rect.move(0, 10)
+                    else:
+                        y_pac -= 10
                 else:
-                    y_pac -= 1
+                    y_pac -= 10
 
             if args[0] == "left":
-                x_pac -= 1
-                if x_pac == -1 and y_pac == 9:
-                    self.rect = self.rect.move(570, 0)
-                    x_pac = 19
-                elif self.level[x_pac][y_pac] != "#":
-                    self.rect = self.rect.move(-30, 0)
+                self.image = pygame.transform.flip(self.image, True, False)
+                x_pac -= 10
+                x_cell = math.floor(x_pac / 30)
+                if x_pac % 30 == 0 and y_pac % 30 == 0:
+                    if self.level[x_pac // 30][y_pac // 30] != "-":
+                        self.level[x_pac // 30][y_pac // 30] = "-"
+                        checker += 1
+                if y_pac % 30 == 0:
+                    y_cell = y_pac // 30
+                    if x_cell == -1 and y_cell == 9:
+                        self.rect = self.rect.move(570, 0)
+                        x_pac = 570
+                        if self.level[x_pac // 30][y_pac // 30] != "-":
+                            self.level[x_pac // 30][y_pac // 30] = "-"
+                            checker += 1
+                    elif self.level[x_cell][y_cell] != "#":
+                        self.rect = self.rect.move(-10, 0)
+                    else:
+                        x_pac += 10
                 else:
-                    x_pac += 1
+                    x_pac += 10
 
             if args[0] == "right":
-                x_pac += 1
-                if x_pac == 20 and y_pac == 9:
-                    self.rect = self.rect.move(-570, 0)
-                    x_pac = 0
-                elif self.level[x_pac][y_pac] != "#":
-                    self.rect = self.rect.move(30, 0)
+                x_pac += 10
+                x_cell = math.ceil(x_pac / 30)
+                if x_pac % 30 == 0 and y_pac % 30 == 0:
+                    if self.level[x_pac // 30][y_pac // 30] != "-":
+                        self.level[x_pac // 30][y_pac // 30] = "-"
+                        checker += 1
+                if y_pac % 30 == 0:
+                    y_cell = y_pac // 30
+                    if x_cell == 20 and y_cell == 9:
+                        self.rect = self.rect.move(-570, 0)
+                        x_pac = 0
+                        if self.level[x_pac // 30][y_pac // 30] != "-":
+                            self.level[x_pac // 30][y_pac // 30] = "-"
+                            checker += 1
+                    elif self.level[x_cell][y_cell] != "#":
+                        self.rect = self.rect.move(10, 0)
+                    else:
+                        x_pac -= 10
                 else:
-                    x_pac -= 1
+                    x_pac -= 10
 
 
 def main():
+    global level
+    pygame.font.init()
+    my_font = pygame.font.SysFont(None, 60)
     pygame.display.set_caption('Клетчатое поле начало')
     screen = pygame.display.set_mode(SIZE)
-    level = load_level("lvl.txt")
 
     Pacman(level, all_sprites)
 
@@ -146,6 +272,12 @@ def main():
     board = Board(20, 20, level)
     board.set_view(25, 75, 30)
     board.render(screen)
+    ghosts = pygame.sprite.Group()
+    blue_ghost_image = load_image('blueghost.png', colorkey=-1)
+    red_ghost_image = load_image('redghost.png', colorkey=-1)
+    blue_ghost = Ghost(300, 290, (0, 0, 255), blue_ghost_image)
+    red_ghost = Ghost(60, 560, (255, 0, 0), red_ghost_image)
+    ghosts.add(blue_ghost, red_ghost)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -154,18 +286,32 @@ def main():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            all_sprites.update("left")
+            all_sprites.update(screen, "left")
         elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            all_sprites.update("right")
+            all_sprites.update(screen, "right")
         elif keys[pygame.K_UP] or keys[pygame.K_w]:
-            all_sprites.update("up")
+            all_sprites.update(screen, "up")
         elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            all_sprites.update("down")
+            all_sprites.update(screen, "down")
         all_sprites.draw(screen)
+        for ghost in ghosts:
+            ghost.upd(level, screen)
+        ghosts.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
         screen.fill("black")
-        board.render(screen)
+        if checker == 168:
+            text_surface = my_font.render('Ты выиграл!', False, (0, 0, 205))
+            screen.blit(text_surface, (210, 300))
+            pygame.display.update()
+            time.sleep(3)
+            break
+        else:
+            board.render(screen)
+            text_surface = my_font.render('Очков: ' + str(checker) + " из 168", False, (0, 0, 205))
+            screen.blit(text_surface, (280, 30))
+            text_surface2 = my_font.render('Жизней: ' + str(lifes + 1), False, (205, 0, 0))
+            screen.blit(text_surface2, (30, 30))
 
 
 if __name__ == '__main__':
